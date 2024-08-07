25aae7142f93eb6077fe60617c945fc9090313e2
javedalihosamani <javed.hosamani007@gmail.com>
2024-08-07 17:17:02 +0530

    Counter Project Completed

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React, { Fragment, useState } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
-    <Container className='my-5'>
+    <Fragment>
+      <Container className='my-5'>
+          <Row>
+              <Col>
+                  <h1 className="display-5 text-center bg-primary text-white p-3 border-radius rounded">COUNTER COMPONENT</h1>
+              </Col>
+          </Row>
+      </Container>
+
+      <Container className='my-5'>
         <Row>
-            <Col>
-                <h1 className="display-5 text-center bg-primary text-white p-3 border-radius rounded">COUNTER COMPONENT</h1>
-            </Col>
+          <Col>
+            <h1 className='text-center text-primary display-3'>{count}</h1>
+          </Col>
         </Row>
-    </Container>
+        <Row>
+          <Col>
+          <Button variant='success rounded-circle' onClick={increment} >+</Button>
+          <Button variant='danger float-end rounded-circle' onClick={decrement} disabled={count === 0}>-</Button>
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   )
 }
 
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap'
 
 const Todo = () => {
   return (
-    <Container className='my-5'>
+    <>
+      <Container className='my-5'>
+          <Row>
+              <Col>
+                  <h1 className="display-5 text-center bg-primary text-white p-3 border-radius rounded">TODO COMPONENT</h1>
+              </Col>
+          </Row>
+      </Container>
+
+      <Container className='my-5'>
         <Row>
-            <Col>
-                <h1 className="display-5 text-center bg-primary text-white p-3 border-radius rounded">TODO COMPONENT</h1>
-            </Col>
+          <Col>
+            <Card className="p-3">
+              <Card.Body>
+                <Form.Group as={Row}>
+                  <Col xs={12} sm={12} md={12} lg={10} xl={10} xxl={10} className='my-2'>
+                    <Form.Control type="text" placeholder="Enter Some Data.."  />
+                  </Col>
+                  <Col xs={12} sm={12} md={12} lg={2} xl={2} xxl={2} className='my-2'>
+                    <Button>Submit</Button>
+                  </Col>
+                </Form.Group>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
-    </Container>
+      </Container>
+    </>
   )
 }
 
